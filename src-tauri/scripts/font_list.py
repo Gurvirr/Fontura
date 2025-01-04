5fdcd5a9f6c8d6613bb79aa0096060854e3ea7e4
Gurvir <gurvirrandhawa28@gmail.com>
2025-01-04 17:29:58 -0500

    Python script updated for correct file path

--- src-tauri/scripts/font_list.py
+++ src-tauri/scripts/font_list.py
@@ -1,5 +1,6 @@
 import matplotlib.font_manager
 import json
+import os
 
 def get_unique_fonts():
     fonts = matplotlib.font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
@@ -9,5 +10,9 @@ def get_unique_fonts():
 fonts = get_unique_fonts()
 font_dict = {"fonts": fonts}
 
-with open('fonts.json', 'w') as json_file:
+tauri_path = os.path.join(os.getcwd(), "public")
+
+os.makedirs(tauri_path, exist_ok=True)
+
+with open(os.path.join(tauri_path, "fonts.json"), "w") as json_file:
     json.dump(font_dict, json_file, indent=4)
